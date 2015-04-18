e3380c9710bab7d171531a95d6958a1bd369f063
Edstr <ed.strambini@gmail.com>
2015-04-18 17:41:18 +0200

    Add create user

---
+++ src/java/facade/PermissionFacade.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facade;
+
+import entity.util.Permission;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Eddy
+ */
+@Stateless
+public class PermissionFacade extends AbstractFacade<Permission> {
+    @PersistenceContext(unitName = "CalendARCPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public PermissionFacade() {
+        super(Permission.class);
+    }
+    
+}
