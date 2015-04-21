2acd272fb02fd52e1d8dee49a4bc34bbb2de98d5
Cyriaque Skrapits <cysoun@gmail.com>
2015-04-21 15:59:40 +0200

    New Database schema

--- db/calendarc.sql
+++ db/calendarc.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.14
+-- version 4.4.2
 -- http://www.phpmyadmin.net
 --
--- Client :  127.0.0.1
--- Généré le :  Mar 21 Avril 2015 à 11:21
--- Version du serveur :  5.6.17
--- Version de PHP :  5.5.12
+-- Client :  localhost
+-- Généré le :  Mar 21 Avril 2015 à 15:56
+-- Version du serveur :  10.0.17-MariaDB-log
+-- Version de PHP :  5.6.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -27,11 +27,9 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `admins` (
-  `idadmin` int(11) NOT NULL AUTO_INCREMENT,
-  `iduser` int(11) DEFAULT NULL,
-  PRIMARY KEY (`idadmin`),
-  UNIQUE KEY `iduser_UNIQUE` (`iduser`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+  `idadmin` int(11) NOT NULL,
+  `iduser` int(11) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 --
 -- Contenu de la table `admins`
@@ -47,11 +45,10 @@ INSERT INTO `admins` (`idadmin`, `iduser`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `calendars` (
-  `idcalendar` int(11) NOT NULL AUTO_INCREMENT,
+  `idcalendar` int(11) NOT NULL,
   `iduser` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  PRIMARY KEY (`idcalendar`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -60,20 +57,17 @@ CREATE TABLE IF NOT EXISTS `calendars` (
 --
 
 CREATE TABLE IF NOT EXISTS `classes` (
-  `idclass` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(12) NOT NULL,
-  PRIMARY KEY (`idclass`),
-  UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+  `idclass` int(11) NOT NULL,
+  `name` varchar(12) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 --
 -- Contenu de la table `classes`
 --
 
 INSERT INTO `classes` (`idclass`, `name`) VALUES
-(3, 'INF2-A'),
-(65, 'INF3-A'),
-(2, 'INF3-B');
+(1, 'INF1'),
+(2, 'INF2');
 
 -- --------------------------------------------------------
 
@@ -82,32 +76,21 @@ INSERT INTO `classes` (`idclass`, `name`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `events` (
-  `idevent` int(11) NOT NULL AUTO_INCREMENT,
+  `idevent` int(11) NOT NULL,
   `iduser` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `description` text,
   `startdate` datetime NOT NULL,
-  `enddate` datetime NOT NULL,
-  PRIMARY KEY (`idevent`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=20 ;
+  `enddate` datetime NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 --
 -- Contenu de la table `events`
 --
 
 INSERT INTO `events` (`idevent`, `iduser`, `name`, `description`, `startdate`, `enddate`) VALUES
-(4, 5, 'Faire PPT JEE', 'Car faut bien faire un Powerpoint', '2015-04-21 09:30:00', '2015-04-21 10:00:00'),
-(5, 5, 'Présentation JEE', 'Oh yeah !', '2015-04-21 14:45:00', '2015-04-21 15:00:00'),
-(9, 5, 'Cuda Saucisson', 'Fuu', '2015-04-22 08:45:00', '2015-04-22 10:15:00'),
-(10, 5, 'Présentation Bought''n Sold', 'Oh yeah', '2015-04-24 12:45:00', '2015-04-24 12:45:00'),
-(11, 5, 'Workshop Web''', 'Encore ?', '2015-04-23 14:15:00', '2015-04-23 16:50:00'),
-(12, 5, 'Skype P3', 'hmm', '2015-04-20 18:00:00', '2015-04-20 20:00:00'),
-(14, 5, 'Rendre projet C#', 'Car il le faut bien.', '2015-04-27 15:15:00', '2015-04-27 15:15:00'),
-(15, 5, 'Rendre TP IA 3', 'Avec Andy', '2015-04-29 15:15:00', '2015-04-29 15:15:00'),
-(16, 5, 'Fin de semèstre !', 'Murge ? Murge !', '2015-05-01 18:00:00', '2015-05-01 23:00:00'),
-(17, 5, 'Cuda ?', 'Qui sait ?', '2015-05-03 12:00:00', '2015-05-03 12:00:00'),
-(18, 1, 'Réunion administration', 'lel', '2015-04-21 09:45:00', '2015-04-21 12:45:00'),
-(19, 6, 'Corriger Projets C#', ':(', '2015-04-27 15:00:00', '2015-04-27 23:00:00');
+(1, 3, 'Event 1', 'Oui', '2015-04-23 14:00:00', '2015-04-23 20:00:00'),
+(2, 3, 'Event 2', 'Non', '2015-04-21 14:00:00', '2015-04-21 17:00:00');
 
 -- --------------------------------------------------------
 
@@ -116,23 +99,21 @@ INSERT INTO `events` (`idevent`, `iduser`, `name`, `description`, `startdate`, `
 --
 
 CREATE TABLE IF NOT EXISTS `lessons` (
-  `idlesson` int(11) NOT NULL AUTO_INCREMENT,
+  `idlesson` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
   `room` int(11) NOT NULL,
-  `starttime` time NOT NULL,
-  `endtime` time NOT NULL,
-  `day` tinyint(1) NOT NULL,
-  `cycle` tinyint(1) DEFAULT NULL COMMENT '0/null for every week, 1 for even, 2 for odd',
-  PRIMARY KEY (`idlesson`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
+  `period` smallint(6) NOT NULL,
+  `length` smallint(6) NOT NULL,
+  `day` tinyint(2) NOT NULL,
+  `cycle` tinyint(2) DEFAULT NULL COMMENT '0/null for every week, 1 for even, 2 for odd'
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 --
 -- Contenu de la table `lessons`
 --
 
-INSERT INTO `lessons` (`idlesson`, `name`, `room`, `starttime`, `endtime`, `day`, `cycle`) VALUES
-(6, '.Net', 150, '13:45:00', '16:15:00', 0, 0),
-(7, 'C++ QT', 138, '09:45:00', '13:00:00', 0, 0);
+INSERT INTO `lessons` (`idlesson`, `name`, `room`, `period`, `length`, `day`, `cycle`) VALUES
+(3, 'Le fist', 666, 1, 3, 3, 0);
 
 -- --------------------------------------------------------
 
@@ -141,11 +122,9 @@ INSERT INTO `lessons` (`idlesson`, `name`, `room`, `starttime`, `endtime`, `day`
 --
 
 CREATE TABLE IF NOT EXISTS `permission` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `permissionName` varchar(25) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+  `id` int(11) NOT NULL,
+  `permissionName` varchar(25) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `permission`
@@ -163,11 +142,10 @@ INSERT INTO `permission` (`id`, `permissionName`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `rel_calendar_event` (
-  `idrel` int(11) NOT NULL AUTO_INCREMENT,
+  `idrel` int(11) NOT NULL,
   `idcalendar` int(11) NOT NULL,
-  `idevent` int(11) NOT NULL,
-  PRIMARY KEY (`idrel`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  `idevent` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -176,11 +154,10 @@ CREATE TABLE IF NOT EXISTS `rel_calendar_event` (
 --
 
 CREATE TABLE IF NOT EXISTS `rel_calendar_user` (
-  `idrel` int(11) NOT NULL AUTO_INCREMENT,
+  `idrel` int(11) NOT NULL,
   `idcalendar` int(11) NOT NULL,
-  `iduser` int(11) NOT NULL,
-  PRIMARY KEY (`idrel`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  `iduser` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -189,20 +166,17 @@ CREATE TABLE IF NOT EXISTS `rel_calendar_user` (
 --
 
 CREATE TABLE IF NOT EXISTS `rel_class_lesson` (
-  `idrel` int(11) NOT NULL AUTO_INCREMENT,
+  `idrel` int(11) NOT NULL,
   `idclass` int(11) NOT NULL,
-  `idlesson` int(11) NOT NULL,
-  PRIMARY KEY (`idrel`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=11 ;
+  `idlesson` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 --
 -- Contenu de la table `rel_class_lesson`
 --
 
 INSERT INTO `rel_class_lesson` (`idrel`, `idclass`, `idlesson`) VALUES
-(7, 65, 6),
-(8, 2, 6),
-(9, 3, 7);
+(2, 1, 3);
 
 -- --------------------------------------------------------
 
@@ -211,19 +185,17 @@ INSERT INTO `rel_class_lesson` (`idrel`, `idclass`, `idlesson`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `rel_class_user` (
-  `idrel` int(11) NOT NULL AUTO_INCREMENT,
+  `idrel` int(11) NOT NULL,
   `iduser` int(11) NOT NULL,
-  `idclass` int(11) NOT NULL,
-  PRIMARY KEY (`idrel`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+  `idclass` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 --
 -- Contenu de la table `rel_class_user`
 --
 
 INSERT INTO `rel_class_user` (`idrel`, `iduser`, `idclass`) VALUES
-(2, 2, 65),
-(33, 1, 2);
+(1, 3, 1);
 
 -- --------------------------------------------------------
 
@@ -232,11 +204,10 @@ INSERT INTO `rel_class_user` (`idrel`, `iduser`, `idclass`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `rel_event_user` (
-  `idrel` int(11) NOT NULL AUTO_INCREMENT,
+  `idrel` int(11) NOT NULL,
   `idevent` int(11) NOT NULL,
-  `iduser` int(11) NOT NULL,
-  PRIMARY KEY (`idrel`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  `iduser` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -245,11 +216,10 @@ CREATE TABLE IF NOT EXISTS `rel_event_user` (
 --
 
 CREATE TABLE IF NOT EXISTS `rel_lesson_user` (
-  `idrel` int(11) NOT NULL AUTO_INCREMENT,
+  `idrel` int(11) NOT NULL,
   `idlesson` int(11) NOT NULL,
-  `iduser` int(11) NOT NULL,
-  PRIMARY KEY (`idrel`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  `iduser` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -258,11 +228,9 @@ CREATE TABLE IF NOT EXISTS `rel_lesson_user` (
 --
 
 CREATE TABLE IF NOT EXISTS `teachers` (
-  `idteacher` int(11) NOT NULL AUTO_INCREMENT,
-  `iduser` int(11) NOT NULL,
-  PRIMARY KEY (`idteacher`),
-  UNIQUE KEY `iduser` (`iduser`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+  `idteacher` int(11) NOT NULL,
+  `iduser` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 --
 -- Contenu de la table `teachers`
@@ -278,27 +246,22 @@ INSERT INTO `teachers` (`idteacher`, `iduser`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `users` (
-  `iduser` int(11) NOT NULL AUTO_INCREMENT,
+  `iduser` int(11) NOT NULL,
   `firstname` varchar(255) NOT NULL,
   `lastname` varchar(255) NOT NULL,
   `mail` varchar(64) NOT NULL,
   `password` varchar(64) NOT NULL,
-  `permission` int(11) NOT NULL,
-  PRIMARY KEY (`iduser`),
-  UNIQUE KEY `mail` (`mail`),
-  KEY `permission` (`permission`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
+  `permission` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
 --
 -- Contenu de la table `users`
 --
 
 INSERT INTO `users` (`iduser`, `firstname`, `lastname`, `mail`, `password`, `permission`) VALUES
-(1, 'admin', 'admin', 'admin.calendarc@he-arc.ch', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 1),
-(2, 'Simon', 'Guillaume-gentil', 'simon.guillaume@he-arc.ch', '0a5d17d3b19f82f8340d3977609aa9e86b4ad8b9bd71bd9eced9271f1d5b2e4a', 2),
-(3, 'Cyriaque', 'Skrapits', 'cysoun@gmail.com', 'df5bd2e887057a24380b2c60f2822556f534e8ea4cf5f8b74c37966e3c8232b6', 2),
-(5, 'Eddy', 'Strambini', 'eddy.strambini@he-arc.ch', '10344fbb33cf720fe810d0735a7761a89c044b1e828504ae15d8750122edc2c5', 2),
-(6, 'Olivier', 'Husser', 'olivier.husser@he-arc.ch', '152fc4dddcedc0be83ea10cd2667d9e4218b00439c65cb352f4a048e409203b2', 3);
+(1, 'Robert', 'Paulson', 'robert.paulson@he-arc.ch', '4007d46292298e83da10d0763d95d5139fe0c157148d0587aa912170414ccba6', 1),
+(2, 'Simon', 'Guillaume-gentil', 'simon.guillaume@he-arc.ch', '088e16a1019277b15d58faf0541e11910eb756f6', 2),
+(3, 'Jean', 'Jacques', 'jean@jean.com', '4ff17bc8ee5f240c792b8a41bfa2c58af726d83b925cf696af0c811627714c85', 3);
 
 -- --------------------------------------------------------
 
@@ -313,6 +276,7 @@ CREATE TABLE IF NOT EXISTS `userspermissions` (
 ,`password` varchar(64)
 ,`permissionName` varchar(25)
 );
+
 -- --------------------------------------------------------
 
 --
@@ -322,6 +286,174 @@ DROP TABLE IF EXISTS `userspermissions`;
 
 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `userspermissions` AS select `users`.`iduser` AS `idUser`,`users`.`firstname` AS `firstname`,`users`.`lastname` AS `lastname`,`users`.`mail` AS `mail`,`users`.`password` AS `password`,`permission`.`permissionName` AS `permissionName` from (`users` join `permission`) where (`users`.`permission` = `permission`.`id`);
 
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`idadmin`),
+  ADD UNIQUE KEY `iduser_UNIQUE` (`iduser`);
+
+--
+-- Index pour la table `calendars`
+--
+ALTER TABLE `calendars`
+  ADD PRIMARY KEY (`idcalendar`);
+
+--
+-- Index pour la table `classes`
+--
+ALTER TABLE `classes`
+  ADD PRIMARY KEY (`idclass`),
+  ADD UNIQUE KEY `name` (`name`);
+
+--
+-- Index pour la table `events`
+--
+ALTER TABLE `events`
+  ADD PRIMARY KEY (`idevent`);
+
+--
+-- Index pour la table `lessons`
+--
+ALTER TABLE `lessons`
+  ADD PRIMARY KEY (`idlesson`);
+
+--
+-- Index pour la table `permission`
+--
+ALTER TABLE `permission`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Index pour la table `rel_calendar_event`
+--
+ALTER TABLE `rel_calendar_event`
+  ADD PRIMARY KEY (`idrel`);
+
+--
+-- Index pour la table `rel_calendar_user`
+--
+ALTER TABLE `rel_calendar_user`
+  ADD PRIMARY KEY (`idrel`);
+
+--
+-- Index pour la table `rel_class_lesson`
+--
+ALTER TABLE `rel_class_lesson`
+  ADD PRIMARY KEY (`idrel`);
+
+--
+-- Index pour la table `rel_class_user`
+--
+ALTER TABLE `rel_class_user`
+  ADD PRIMARY KEY (`idrel`);
+
+--
+-- Index pour la table `rel_event_user`
+--
+ALTER TABLE `rel_event_user`
+  ADD PRIMARY KEY (`idrel`);
+
+--
+-- Index pour la table `rel_lesson_user`
+--
+ALTER TABLE `rel_lesson_user`
+  ADD PRIMARY KEY (`idrel`);
+
+--
+-- Index pour la table `teachers`
+--
+ALTER TABLE `teachers`
+  ADD PRIMARY KEY (`idteacher`),
+  ADD UNIQUE KEY `iduser` (`iduser`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`iduser`),
+  ADD UNIQUE KEY `mail` (`mail`),
+  ADD KEY `permission` (`permission`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `idadmin` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT pour la table `calendars`
+--
+ALTER TABLE `calendars`
+  MODIFY `idcalendar` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `classes`
+--
+ALTER TABLE `classes`
+  MODIFY `idclass` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT pour la table `events`
+--
+ALTER TABLE `events`
+  MODIFY `idevent` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT pour la table `lessons`
+--
+ALTER TABLE `lessons`
+  MODIFY `idlesson` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT pour la table `permission`
+--
+ALTER TABLE `permission`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT pour la table `rel_calendar_event`
+--
+ALTER TABLE `rel_calendar_event`
+  MODIFY `idrel` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `rel_calendar_user`
+--
+ALTER TABLE `rel_calendar_user`
+  MODIFY `idrel` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `rel_class_lesson`
+--
+ALTER TABLE `rel_class_lesson`
+  MODIFY `idrel` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT pour la table `rel_class_user`
+--
+ALTER TABLE `rel_class_user`
+  MODIFY `idrel` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT pour la table `rel_event_user`
+--
+ALTER TABLE `rel_event_user`
+  MODIFY `idrel` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `rel_lesson_user`
+--
+ALTER TABLE `rel_lesson_user`
+  MODIFY `idrel` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `teachers`
+--
+ALTER TABLE `teachers`
+  MODIFY `idteacher` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `iduser` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
 --
 -- Contraintes pour les tables exportées
 --
